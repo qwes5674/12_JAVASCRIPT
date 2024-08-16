f8641c6e578f1ef182d4ae17fbca50931c7459d5
qwes5674 <qwes5674@gmail.com>
2024-08-16 13:17:10 +0900

    JavaScript

---
+++ 03_EB6+/04_spread-syntax/01_rest-parameter.js
@@ -0,0 +1,26 @@
+// rest Paramter (나머지 매개변수) ...
+
+function merge(msg1, msg2) {
+    return msg1 + msg2;
+}
+
+console.log(merge('안녕하세요.'));
+console.log(merge('안녕하세요.', '반갑습니다.'));
+console.log(merge('안녕하세요.', '반갑습니다.', '제이름은 홍길동 입니다.'));
+
+function mergeAll(...args) {
+    let message = '';
+
+    for(let arg of args) message += arg;
+
+    return message;
+}
+
+console.log(mergeAll('안녕하세요.'));
+console.log(mergeAll('안녕하세요.', '반갑습니다.'));
+console.log(mergeAll('안녕하세요.', '반갑습니다.', '제이름은 홍길동 입니다.'));
+
+// 나머지 매개변수(...) 은 항상 마지막에 있어야 한다.
+// function func(arg1, ... args, arg2) {} => (x)
+// function func(arg1, args2, ...arg) {} => (o)
+
